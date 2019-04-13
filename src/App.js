4c5ab5eb20a97620d8efb6dcb111b44e4fa1d523
Paula Le <paula.cnhcki@gmail.com>
2019-04-13 15:48:07 -0700

    Fixed stuff and some styling changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 import React, { Component } from 'react';
-import FileUpload from './components/firebase/FileUpload'
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <FileUpload />
-        </header>
-      </div>
-    );
-  }
-
 import { BrowserRouter, Route } from "react-router-dom";
 import LandingPage from './landingpage/LandingPage';
+import FileUpload from './components/firebase/FileUpload';
 import NavBar from './navigation/NavBar';
 import './App.css';
 
